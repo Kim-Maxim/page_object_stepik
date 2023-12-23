0c78287d7e7bcedea3f4dedbdbfabbc67f8363e7
Maxim Kim <kimmaxim69@gmail.com>
2023-12-23 13:55:19 +0300

    Добавил опций при открытии браузера

--- conftest.py
+++ conftest.py
@@ -5,22 +5,31 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en', help='Choose language')
+    parser.addoption("--language", action="store", default="en", help="Choose language")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption('language')
+    user_language = request.config.getoption("language")
     chrome_options = Options()
-    chrome_options.page_load_strategy = 'eager'
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.page_load_strategy = "eager"
     chrome_options.add_argument("--headless")
-    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    chrome_options.set_capability("browserVersion", "118")
+    chrome_options.add_argument("--log-level=1")
+    chrome_options.add_experimental_option(
+        "prefs", {"intl.accept_languages": user_language}
+    )
     browser = webdriver.Chrome(options=chrome_options)
     browser.implicitly_wait(10)
     browser.maximize_window()
     yield browser
     attach = browser.get_screenshot_as_png()
-    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG) 
+    allure.attach(
+        attach,
+        name=f"Screenshot {datetime.today()}",
+        attachment_type=allure.attachment_type.PNG,
+    )
     browser.quit()
-    
